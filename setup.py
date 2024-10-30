2f9e943118685c256fe8b0b56908021d4cbf6d63
MohammoD2 <ibr500037@gmail.com>
2024-10-30 14:23:00 +0600

    inisilizing prosess

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This real estate platform enables users to estimate flat prices based on personalized criteria like location, amenities, and property size, offering valuable analysis and recommendations for informed decision-making. It incorporates advanced skills in data analysis, machine learning, and MLOps, utilizing DVC for data version control and efficient model tracking. This approach ensures accuracy, seamless experiment management, and reproducibility, providing users with reliable insights into the property market.',
+    author='Mohammod Ibrahim Hossain',
+    license='MIT',
+)
